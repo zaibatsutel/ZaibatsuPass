66f8e7808f45f74e479a9b56d33025932d620007
Morgan Gangwere <morgan.gangwere@gmail.com>
2016-07-13 19:28:11 -0700

    MYKI: Initial support (not yet parsable)
    Incl. stub for myki + Luhn checksum.

---
+++ ZaibatsuPass/StringExtensions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZaibatsuPass
+{
+    static class StringExtensions
+    {
+
+        public static char CalculateLuhnChecksum(this string self)
+        {
+            int sum = 00;
+            // get the Luhn checksum
+            for(int idx=0; idx < self.Length; idx++)
+            {
+                int tmp;
+                bool result = int.TryParse("" + self[idx], out tmp);
+                if (!result) throw new FormatException("String is not exclusively series of numeric values: " + self);
+
+                if ((idx & 0x01) == 00)
+                {
+                    // Odd digit. Simply add the parsed value
+                    sum += tmp;
+                }
+                else
+                {
+                    // even digit. Double it
+                    int tmp2 = tmp * 2;
+                    if (tmp2 > 9)
+                    {
+                        // get the two digits and add to the sum individually.
+                        // we can cheat here because the maximum value we'll get out is 9*2 (18) which means we can do integer division
+                        // and get the 10's place, subtract 10 and get the ones place. 
+                        sum += ( tmp2 / 10 ) + ( tmp2 - 10 );
+
+                    }
+                    else
+                        sum += tmp2;
+                }
+            }
+
+
+
+            // now that we have the sum, we mod by 10 (
+
+            int check = 10 - (sum % 10);
+
+            return (check.ToString()[0]);
+        }
+
+    }
+}
---
+++ ZaibatsuPass/TransitCard/Stub/Myki/MykiCard.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZaibatsuPass.PhysicalCard;
+
+namespace ZaibatsuPass.TransitCard.Stub.Myki
+{
+    class MykiCard : ZaibatsuPass.TransitCard.TransitCard
+    {
+
+        public readonly static byte[] AID_MYKI_A = { 0x00, 0x11, 0xF2 };
+        public readonly static byte[] AID_MYKI_B = { 0xF0, 0x10, 0xF2 };
+
+        private long serialLower = 0;
+        private long serialUpper = 0;
+
+        public MykiCard(PhysicalCard.PhysicalCard c) : base(c)
+        {
+            if(c is PhysicalCard.Desfire.DesfireCard)
+            {
+                PhysicalCard.Desfire.DesfireCard dCard = (c as PhysicalCard.Desfire.DesfireCard);
+                try
+                {
+                    byte[] meta = dCard.getApplication(AID_MYKI_A).getFile(15).Data.CopyReverseFrom(0,16);
+                    serialUpper = meta.getBitsFromBuffer(96, 32);
+                    serialLower = meta.getBitsFromBuffer(64, 32);
+                }
+                catch
+                {
+                    throw new ArgumentException("Could not parse Myki data.");
+                }
+            }
+            else
+            {
+                throw new ArgumentException("Not a Myki card");
+            }
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return "Myki";
+            }
+        }
+
+        public override bool isStub
+        {
+            get
+            {
+                return true;
+            }
+        }
+        public override string SerialNumber
+        {
+            get
+            {
+                return String.Format("{0:6D}{1:8D}{2}", serialUpper, serialLower, "X");
+            }
+        }
+    }
+}
